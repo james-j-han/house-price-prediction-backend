92b4875e7f411bfe8439af99d1c76f95a7b58021
James Han <jameshan@jamess-mbp.lan>
2025-04-21 21:19:14 -0400

    deployment

--- app.py
+++ app.py
@@ -142,4 +142,5 @@ def get_feature_weights():
     return jsonify({"weights": weights})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
